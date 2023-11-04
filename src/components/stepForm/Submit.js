2c8a919b522ff210ab47905bb1defbbdbdd5b835
R-monisha2000 <141928876+R-monisha2000@users.noreply.github.com>
2023-11-04 19:57:18 +0530

    Add files via upload

---
+++ src/components/stepForm/Submit.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Container from '@material-ui/core/Container';
+
+export const Submit = () => {
+  return (
+    <Container maxWidth="sm" style={{ marginTop: '4rem' }}>
+      <h3>Thank you for submitting</h3>
+      
+    </Container>
+  );
+};
